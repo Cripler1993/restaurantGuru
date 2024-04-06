482a54e88d392a08c7a1af058e035ddf0a7e36f5
Cripler1993 <cripler1993@mail.ru>
2024-04-06 12:36:37 +0300

    add slider and js logic for slider

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  // Default parameters
+  slidesPerView: 1.1,
+  spaceBetween: 20,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    614: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 18,
+    },
+
+    1225: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next-img",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 let locationContainer = document.querySelector(".location__container");
 let locationTemplate = document.querySelector("#language");
 let mainLocationFlag = document.querySelector(".mainFlag");
+let locationContainerLanguages = document.querySelector(
+  ".location__container-languages"
+);
+let closeBtn = document.querySelector(".location__container-close");
 
 let languages = [
   { name: "russian", src: "./img/russiaFlag.png", selected: true },
@@ -9,42 +46,52 @@ let languages = [
 ];
 
 function renderLanguages(arr) {
+  locationContainerLanguages.innerHTML = "";
   arr.forEach((elem) => {
     if (!elem.selected) {
       let language = locationTemplate.content.cloneNode(true);
 
       let languageFlag = language.querySelector(".languageFlag");
       let languageName = language.querySelector(".languageName");
-      let cloneLocation = language.querySelectorAll(".location");
-
-      cloneLocationLocation.forEach((element, index) => {
-        element.addEventListener("click", function (event) {
-          console.log(event.target);
-          console.log(index);
-          if (!event.target) {
-            elem.selected = false;
-          } else {
-            elem.selected = true;
-          }
-          console.log(languages);
-        });
-      });
+      let cloneLocation = language.querySelector(".location");
 
       languageName.innerHTML = elem.name;
       languageFlag.src = elem.src;
 
-      locationContainer.append(language);
+      cloneLocation.addEventListener("click", function () {
+        arr.forEach((element) => {
+          if (elem.name == element.name) {
+            element.selected = true;
+          } else {
+            element.selected = false;
+          }
+        });
+        renderLanguages(arr);
+      });
+
+      locationContainerLanguages.append(language);
     } else {
-      mainLocationFlag = elem.src;
+      mainLocationFlag.src = elem.src;
     }
   });
 }
 renderLanguages(languages);
 
-// function openDropDown() {
-//   mainLocationFlag.addEventListener("click", function () {
-//     locationContainer.classList.add("active");
-//   });
-// }
-// openDropDown();
-// закрытие по всему окну и крестику
+mainLocationFlag.addEventListener("click", function () {
+  locationContainer.classList.add("location__container-active");
+});
+
+function closeDropDown() {
+  locationContainer.classList.remove("location__container-active");
+}
+
+closeBtn.addEventListener("click", closeDropDown);
+
+document.body.addEventListener("click", function (event) {
+  if (
+    !event.target.closest(".mainFlag") &&
+    !event.target.closest(".location__container")
+  ) {
+    closeDropDown();
+  }
+});
